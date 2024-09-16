dd9550da7ae5940e6e3fa3be58c3e449a1b2d2ab
Dmytro <d.cherednychenko35@gmail.com>
2024-09-16 12:00:39 +0300

    Init

--- src/main/java/org/example/MathDemo.java
+++ src/main/java/org/example/MathDemo.java
@@ -3,8 +3,8 @@ package org.example;
 public class MathDemo {
     public static void main(String[] args) {
         double x = 8.1, a = 2.5, b = 7.7, c = -4.32;
-        double result = (Math.exp(a * Math.cos(x+2))) - ((Math.exp(- Math.sin(b * x)))/(c - Math.cbrt(x)));
+        double y = (Math.exp(a * Math.cos(x+2))) - ((Math.exp(- Math.sin(b * x)))/(c - Math.cbrt(x)));
 
-        System.out.printf("Result: %.3f\n", result);
+        System.out.printf("Result: %.3f\n", y);
     }
 }
